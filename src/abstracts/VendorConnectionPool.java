8e814292a98c48755e70c5110a48c28664a6c191
didizlatkova <didi.zlatkova93@gmail.com>
2015-05-18 16:09:17 +0300

    changed some logging messages

--- src/abstracts/VendorConnectionPool.java
+++ src/abstracts/VendorConnectionPool.java
@@ -10,29 +10,39 @@ public abstract class VendorConnectionPool {
 
 	final List<VendorConnection> available;
 	final List<VendorConnection> inUse;
-	
-	public VendorConnectionPool(List<VendorConnection> available, List<VendorConnection> inUse){
+
+	public VendorConnectionPool(List<VendorConnection> available,
+			List<VendorConnection> inUse) {
 		this.available = available;
 		this.inUse = inUse;
 	}
 
 	public VendorConnection getConnection() throws Exception {
 		synchronized (available) {
-			if (available.size() == 0 && inUse.size() < MAX_CONNECTION_COUNT) {
-				this.addConnectionToPool();
-				System.out.println("Debug: Added new connection");
+			if (available.size() == 0) {
+				if (inUse.size() < MAX_CONNECTION_COUNT) {
+					this.addConnectionToPool();
+					System.out.println("Debug: Added new connection to "
+							+ this.getClass().getSimpleName());
+				} else {
+					System.out.println("Debug: Maximum connections reached in "
+							+ this.getClass().getSimpleName());
+				}
+
 			}
-			
+
 			if (available.size() != 0) {
 				VendorConnection connection = available.get(0);
 				inUse.add(connection);
 				available.remove(0);
 
-				System.out.println("Debug: Connections left in the pool: "
+				System.out.println("Debug: Connections left in "
+						+ this.getClass().getSimpleName() + ": "
 						+ available.size());
 				return connection;
 			} else {
-				throw new Exception("No connections are available");
+				throw new Exception("No connections available in "
+						+ this.getClass().getSimpleName());
 			}
 		}
 	}
@@ -43,8 +53,10 @@ public abstract class VendorConnectionPool {
 		synchronized (available) {
 			available.add(connection);
 			inUse.remove(connection);
-			System.out.println("Debug: Connections Left in the pool: "
-					+ available.size());
+			System.out
+					.println("Debug: Connections Left in "
+							+ this.getClass().getSimpleName() + ": "
+							+ available.size());
 		}
 	}
 
